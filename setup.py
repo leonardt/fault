4e682a1f556ba9ebe1a10a06306d9a0a2b47d394
Lenny Truong <lenny@stanford.edu>
2018-04-12 13:54:02 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import sys
+
+setup(
+    name='fault',
+    version='0.1',
+    description='A Python package for testing hardware (part of the magma ecosystem)',
+    scripts=[],
+    packages=[
+        "fault",
+    ],
+    license='BSD License',
+    url='https://github.com/leonardt/fault',
+    author='Leonard Truong',
+    author_email='lenny@cs.stanford.edu'
+    python_requires='>=3.6'
+)
