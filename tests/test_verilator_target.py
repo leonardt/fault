07dd7d7c63c7842a289f50f32a0251e5c04c4fb4
Raj Setaluri <raj.setaluri@gmail.com>
2018-07-19 13:24:25 -0700

    Move target specific logic into own class
    Added the following:
      -- target.Target is the base class for all targets. It contains a very
         simple interface (__init__(), run()).
      -- verilator_target.Target is an implementation of target.Target for
         verilator.
    
    The verilator specific logic in tester.compile_and_run() should now go
    in a separate file for each target.
    
    Also added relevant tests.

---
+++ tests/test_verilator_target.py
@@ -0,0 +1,22 @@
+import magma as m
+import fault
+from bit_vector import BitVector
+
+
+def test_verilator_target():
+
+    class Foo(m.Circuit):
+        IO = ["I", m.In(m.Bit), "O", m.Out(m.Bit)]
+
+        @classmethod
+        def definition(io):
+            m.wire(io.I, io.O)
+
+    # Compile to verilog.
+    # TODO(rsetaluri): Make this part of the target itself.
+    m.compile("build/Foo", Foo, output="verilog")
+
+    test_vectors = [[BitVector(0,1), BitVector(0, 1)]]
+    target = fault.verilator_target.VerilatorTarget(
+        Foo, test_vectors, directory="build/")
+    target.run()
