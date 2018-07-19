ef6bb40866188a7336997df30d9f282292293428
Raj Setaluri <raj.setaluri@gmail.com>
2018-07-19 13:56:45 -0700

    Use tempdir instead of build/

--- tests/test_verilator_target.py
+++ tests/test_verilator_target.py
@@ -1,9 +1,13 @@
+import tempfile
 import magma as m
 import fault
 from bit_vector import BitVector
 
 
 def test_verilator_target():
+    """
+    Test basic verilator workflow with a simple circuit.
+    """
 
     class Foo(m.Circuit):
         IO = ["I", m.In(m.Bit), "O", m.Out(m.Bit)]
@@ -12,11 +16,12 @@ def test_verilator_target():
         def definition(io):
             m.wire(io.I, io.O)
 
-    # Compile to verilog.
-    # TODO(rsetaluri): Make this part of the target itself.
-    m.compile("build/Foo", Foo, output="verilog")
+    with tempfile.TemporaryDirectory() as tempdir:
+        # Compile to verilog.
+        # TODO(rsetaluri): Make this part of the target itself.
+        m.compile(f"{tempdir}/Foo", Foo, output="verilog")
 
-    test_vectors = [[BitVector(0, 1), BitVector(0, 1)]]
-    target = fault.verilator_target.VerilatorTarget(
-        Foo, test_vectors, directory="build/")
-    target.run()
+        test_vectors = [[BitVector(0, 1), BitVector(0, 1)]]
+        target = fault.verilator_target.VerilatorTarget(
+            Foo, test_vectors, directory=f"{tempdir}/")
+        target.run()
