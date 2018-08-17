1c30940e34855f078e0f7ec91d57b67d1f98f9e9
Raj Setaluri <raj.setaluri@gmail.com>
2018-08-16 23:18:39 -0700

    Refactor tester tests
    tests/test_tester.py only tests how Tester produces actions (not
    serialization). The serialization is tested specifically by testing
    TestVectorBuilder (in tests/test_test_vector_builder.py).

---
+++ tests/test_test_vector_builder.py
@@ -0,0 +1,49 @@
+import random
+from bit_vector import BitVector
+import fault
+import common
+from fault.actions import Poke, Expect, Eval, Step
+from fault.array import Array
+from fault.test_vector_builder import TestVectorBuilder
+
+
+def test_tester_basic():
+    circ = common.TestBasicCircuit
+    builder = TestVectorBuilder(circ)
+    builder.process(Poke(circ.I, BitVector(0, 1)))
+    builder.process(Expect(circ.O, BitVector(0, 1)))
+    assert builder.vectors == [[BitVector(0, 1), BitVector(0, 1)]]
+    builder.process(Eval())
+    assert builder.vectors == [[BitVector(0, 1), BitVector(0, 1)],
+                               [BitVector(0, 1), fault.AnyValue]]
+
+
+def test_tester_clock():
+    circ = common.TestBasicClkCircuit
+    builder = TestVectorBuilder(circ)
+    builder.process(Poke(circ.I, BitVector(0, 1)))
+    builder.process(Expect(circ.O, BitVector(0, 1)))
+    assert builder.vectors == [
+        [BitVector(0, 1), BitVector(0, 1), fault.AnyValue]
+    ]
+    builder.process(Poke(circ.CLK, BitVector(0, 1)))
+    assert builder.vectors == [
+        [BitVector(0, 1), BitVector(0, 1), BitVector(0, 1)]
+    ]
+    builder.process(Step(1, circ.CLK))
+    assert builder.vectors == [
+        [BitVector(0, 1), BitVector(0, 1), BitVector(0, 1)],
+        [BitVector(0, 1), fault.AnyValue, BitVector(1, 1)]
+    ]
+
+
+def test_tester_nested_arrays():
+    circ = common.TestNestedArraysCircuit
+    builder = TestVectorBuilder(circ)
+    expected = []
+    for i in range(3):
+        val = random.randint(0, (1 << 4) - 1)
+        builder.process(Poke(circ.I[i], BitVector(val, 4)))
+        builder.process(Expect(circ.O[i], BitVector(val, 4)))
+        expected.append(val)
+    assert builder.vectors == [[Array(expected, 3), Array(expected, 3)]]
--- tests/test_tester.py
+++ tests/test_tester.py
@@ -1,8 +1,13 @@
-import magma as m
-import fault
-from bit_vector import BitVector
 import random
+from bit_vector import BitVector
+import fault
+from fault.actions import Poke, Expect, Eval, Step
 import common
+
+
+def check(got, expected):
+    assert type(got) == type(expected)
+    assert got.__dict__ == expected.__dict__
 
 
 def test_tester_basic():
@@ -10,10 +15,10 @@ def test_tester_basic():
     tester = fault.Tester(circ)
     tester.poke(circ.I, 0)
     tester.expect(circ.O, 0)
-    assert tester.serialize() == [[BitVector(0, 1), BitVector(0, 1)]]
+    check(tester.actions[0], Poke(circ.I, 0))
+    check(tester.actions[1], Expect(circ.O, 0))
     tester.eval()
-    assert tester.serialize() == [[BitVector(0, 1), BitVector(0, 1)],
-                                   [BitVector(0, 1), fault.AnyValue]]
+    check(tester.actions[2], Eval())
 
 
 def test_tester_clock():
@@ -21,18 +26,12 @@ def test_tester_clock():
     tester = fault.Tester(circ, circ.CLK)
     tester.poke(circ.I, 0)
     tester.expect(circ.O, 0)
-    assert tester.serialize() == [
-        [BitVector(0, 1), BitVector(0, 1), fault.AnyValue]
-    ]
+    check(tester.actions[0], Poke(circ.I, 0))
+    check(tester.actions[1], Expect(circ.O, 0))
     tester.poke(circ.CLK, 0)
-    assert tester.serialize() == [
-        [BitVector(0, 1), BitVector(0, 1), BitVector(0, 1)]
-    ]
+    check(tester.actions[2], Poke(circ.CLK, 0))
     tester.step()
-    assert tester.serialize() == [
-        [BitVector(0, 1), BitVector(0, 1), BitVector(0, 1)],
-        [BitVector(0, 1), fault.AnyValue, BitVector(1, 1)]
-    ]
+    check(tester.actions[3], Step(1, circ.CLK))
 
 
 def test_tester_nested_arrays():
@@ -43,7 +42,7 @@ def test_tester_nested_arrays():
         val = random.randint(0, (1 << 4) - 1)
         tester.poke(circ.I[i], val)
         tester.expect(circ.O[i], val)
-        expected.append(val)
-    assert tester.serialize() == [
-         [fault.array.Array(expected, 3), fault.array.Array(expected, 3)]
-    ]
+        expected.append(Poke(circ.I[i], val))
+        expected.append(Expect(circ.O[i], val))
+    for i, exp in enumerate(expected):
+        check(tester.actions[i], exp)
