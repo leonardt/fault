42ccf063138961b711154fb08e4da68bcc50c930
Lenny Truong <lenny@stanford.edu>
2018-08-17 10:34:17 -0700

    Add test for verilator target with test vectors

--- tests/test_test_vectors.py
+++ tests/test_test_vectors.py
@@ -12,3 +12,31 @@ def test_circuit(Circuit):
     function_test_vectors = generate_function_test_vectors(Circuit, fn)
     simulator_test_vectors = generate_simulator_test_vectors(Circuit)
     assert function_test_vectors == simulator_test_vectors
+
+
+def test_basic_circuit():
+    def f(a, b, c):
+        return (a & b) ^ c
+
+    import magma as m
+    import mantle
+
+    class main(m.Circuit):
+        IO = ["a", m.In(m.Bit), "b", m.In(m.Bit), "c", m.In(m.Bit),
+              "d", m.Out(m.Bit)]
+
+        @classmethod
+        def definition(io):
+            m.wire(f(io.a, io.b, io.c), io.d)
+
+    import shutil
+    import os
+    os.mkdir("build")
+    m.compile("build/main", main, "coreir-verilog")
+
+    from fault.verilator_target import VerilatorTarget
+
+    test_vectors = generate_function_test_vectors(main, f)
+
+    VerilatorTarget(main, test_vectors).run()
+    shutil.rmtree("build")
