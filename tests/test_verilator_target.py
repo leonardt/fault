d363fa08396882dc73c4a7a5507dc276d0ceb483
Teguh Hofstee <teguhhofstee@gmail.com>
2018-10-24 21:36:19 +0000

    add test for '--trace'

--- tests/test_verilator_target.py
+++ tests/test_verilator_target.py
@@ -7,7 +7,7 @@ import random
 from fault.actions import Poke, Expect, Eval, Step, Print, Peek
 from fault.random import random_bv
 import copy
-
+import os.path
 
 def run(circ, actions, flags=[]):
     with tempfile.TemporaryDirectory() as tempdir:
@@ -104,3 +104,28 @@ def test_verilator_target_tuple():
         Expect(circ.O.b, 11),
     ]
     run(circ, actions)
+
+def test_verilator_trace():
+    circ = common.TestBasicClkCircuit
+    actions = [
+        Poke(circ.I, 0),
+        Print(circ.I),
+        Expect(circ.O, 0),
+        Poke(circ.CLK, 0),
+        Print(circ.O),
+        Step(circ.CLK, 1),
+        Poke(circ.I, BitVector(1, 1)),
+        Eval(),
+        Print(circ.O),
+    ]
+    flags=["-Wno-lint", "--trace"]
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        assert not os.path.isfile(f"{tempdir}/logs/BasicClkCircuit.vcd"), "Expected logs to be empty"
+        m.compile(f"{tempdir}/{circ.name}", circ,
+                  output="coreir-verilog")
+        target = fault.verilator_target.VerilatorTarget(
+            circ, directory=f"{tempdir}/",
+            flags=flags, skip_compile=True)
+        target.run(actions)
+        assert os.path.isfile(f"{tempdir}/logs/BasicClkCircuit.vcd"), "Expected VCD to exist"
