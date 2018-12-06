43b820dfcfdf280f009538214bf7259dbf22ee05
Lenny Truong <lenny@stanford.edu>
2018-12-05 20:13:07 -0800

    Bump version for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 setup(
     name='fault',
-    version='0.40',
+    version='0.41',
     description='A Python package for testing hardware (part of the magma ecosystem)',
     scripts=[],
     packages=[
