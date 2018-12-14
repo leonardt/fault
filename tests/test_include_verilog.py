7f4f9d72a23b5f0f210fe5d6b338b1266b23bf4c
Lenny Truong <lenny@stanford.edu>
2018-12-13 17:13:26 -0800

    Update include verilog test to include system verilog target

--- tests/test_include_verilog.py
+++ tests/test_include_verilog.py
@@ -5,7 +5,16 @@ import magma as m
 import os
 
 
-def test_include_verilog():
+def pytest_generate_tests(metafunc):
+    if "target" in metafunc.fixturenames:
+        targets = [("verilator", None)]
+        if not os.getenv("TRAVIS", False):
+            targets.append(("system-verilog", "ncsim"))
+            targets.append(("system-verilog", "vcs"))
+        metafunc.parametrize("target,simulator", targets)
+
+
+def test_include_verilog(target, simulator):
     SB_DFF = m.DeclareCircuit('SB_DFF', "D", m.In(m.Bit), "Q", m.Out(m.Bit),
                               "C", m.In(m.Clock))
     main = m.DefineCircuit('main', "I", m.In(m.Bit), "O", m.Out(m.Bit),
@@ -22,13 +31,18 @@ def test_include_verilog():
     tester.step(2)
     tester.expect(main.O, 1)
     sb_dff_filename = pathlib.Path("tests/sb_dff_sim.v").resolve()
+
+    kwargs = {}
+    if simulator is not None:
+        kwargs["simulator"] = simulator
+
     with tempfile.TemporaryDirectory() as tmp_dir:
-        tester.compile_and_run(target="verilator", directory=tmp_dir,
-                               include_verilog_libraries=[sb_dff_filename])
+        tester.compile_and_run(target=target, directory=tmp_dir,
+                               include_verilog_libraries=[sb_dff_filename], **kwargs)
 
     # Should work by including the tests/ directory which contains the verilog
     # file SB_DFF.v
     dir_path = os.path.dirname(os.path.realpath(__file__))
     with tempfile.TemporaryDirectory() as tmp_dir:
-        tester.compile_and_run(target="verilator", directory=tmp_dir,
-                               include_directories=[dir_path])
+        tester.compile_and_run(target=target, directory=tmp_dir,
+                               include_directories=[dir_path], **kwargs)
