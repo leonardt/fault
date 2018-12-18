85633e77d2ec2081ada8b58734cca76a3e4acbe0
Lenny Truong <lenny@stanford.edu>
2018-12-17 20:43:13 -0800

    Release 0.42

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 setup(
     name='fault',
-    version='0.41',
+    version='0.42',
     description='A Python package for testing hardware (part of the magma ecosystem)',
     scripts=[],
     packages=[
