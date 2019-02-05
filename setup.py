c3bacce6a3ed4153fb817b5a0cc79b4f97a5b8e5
Lenny Truong <lenny@stanford.edu>
2019-02-04 18:58:43 -0800

    Add README as long description

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 import sys
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='fault',
     version='0.43',
@@ -16,5 +19,7 @@ setup(
     url='https://github.com/leonardt/fault',
     author='Leonard Truong',
     author_email='lenny@cs.stanford.edu',
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    long_description=long_description,
+    long_description_content_type="text/markdown"
 )
