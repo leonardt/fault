0328d90d7b6cdb84c54ebaa5c6e7202d6bbd2b11
Lenny Truong <lenny@stanford.edu>
2019-02-05 17:52:08 -0800

    Fix regression from change to self.circuit

--- fault/functional_tester.py
+++ fault/functional_tester.py
@@ -28,14 +28,14 @@ class FunctionalTester(fault.Tester):
 
     def eval(self):
         super().eval()
-        for name, port in self.circuit.interface.ports.items():
+        for name, port in self._circuit.interface.ports.items():
             if port.isinput():
-                fn_model_port = get_renamed_port(self.circuit, name)
+                fn_model_port = get_renamed_port(self._circuit, name)
                 super().expect(port, getattr(self.functional_model,
                                              fn_model_port))
 
     def expect_any_outputs(self):
-        for name, port in self.circuit.interface.ports.items():
+        for name, port in self._circuit.interface.ports.items():
             if port.isinput():
-                fn_model_port = get_renamed_port(self.circuit, name)
+                fn_model_port = get_renamed_port(self._circuit, name)
                 setattr(self.functional_model, fn_model_port, AnyValue)
