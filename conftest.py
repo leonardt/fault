fba2da73948a95912707cc2723eafacbfcb7ccca
Lenny Truong <lenny@stanford.edu>
2019-02-08 09:03:55 -0800

    Remove old magma database call

--- conftest.py
+++ conftest.py
@@ -1,5 +1,4 @@
 import pytest
-from magma.circuit import magma_clear_circuit_database
 from magma import clear_cachedFunctions
 import magma.backend.coreir_ as coreir_
 
@@ -8,6 +7,5 @@ collect_ignore = ["src"]  # pip folder that contains dependencies like magma
 
 @pytest.fixture(autouse=True)
 def magma_test():
-    magma_clear_circuit_database()
     clear_cachedFunctions()
     coreir_.__reset_context()
