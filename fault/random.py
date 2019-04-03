1598d96cd80bae669d8e80a3e30bccafb3a1ca86
Caleb Donovick <donovick@cs.stanford.edu>
2019-04-03 12:30:04 -0700

    Move constrained random to random.py

--- fault/random.py
+++ fault/random.py
@@ -2,6 +2,12 @@ import random
 from hwtypes import BitVector, Bit
 
 
+def constrained_random_bv(width, pred):
+    while True:
+        randval = random_bv(width)
+        if pred(randval):
+            return randval
+
 def random_bv(width):
     return BitVector[width](random.randint(0, (1 << width) - 1))
 
