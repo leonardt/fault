a4068ae1691fdc86f0ecdd6c4c38ecc6d3418179
Caleb Donovick <donovick@cs.stanford.edu>
2019-04-26 12:59:40 -0700

    Remove dupeplicate code

--- fault/random.py
+++ fault/random.py
@@ -13,15 +13,8 @@ def constrained_random_bv(width, pred):
         if pred(randval):
             return randval
 
-def constrained_random_bv(width, pred):
-    while True:
-        randval = random_bv(width)
-        if pred(randval):
-            return randval
-
 def random_bv(width):
     return BitVector[width](random.randint(0, (1 << width) - 1))
-
 
 def random_bit():
     return Bit(random.randint(0, 1))
