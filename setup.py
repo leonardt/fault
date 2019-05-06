734c03de9d0ec626b799e2db7b8066702f3e1979
Lenny Truong <lenny@stanford.edu>
2019-05-06 14:39:07 -0700

    Bump version to test travis release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ A Python package for testing hardware (part of the magma ecosystem)\
 
 setup(
     name='fault',
-    version='2.0.5',
+    version='2.0.6',
     description=description,
     scripts=[],
     packages=[
