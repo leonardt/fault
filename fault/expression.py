0380b5dbe337356978ccefe8c8f2dfd0359573ce
Lenny Truong <lenny@stanford.edu>
2019-05-29 19:59:39 -0700

    Add support for nested expressions

--- fault/expression.py
+++ fault/expression.py
@@ -1,5 +1,63 @@
 class Expression:
-    pass
+    def __and__(self, other):
+        return And(self, other)
+
+    def __eq__(self, other):
+        return EQ(self, other)
+
+    def __ne__(self, other):
+        return NE(self, other)
+
+    def __lt__(self, other):
+        return LT(self, other)
+
+    def __le__(self, other):
+        return LE(self, other)
+
+    def __gt__(self, other):
+        return GT(self, other)
+
+    def __ge__(self, other):
+        return GE(self, other)
+
+    def __add__(self, other):
+        return Add(self, other)
+
+    def __invert__(self):
+        return Invert(self)
+
+    def __lshift__(self, other):
+        return LShift(self, other)
+
+    def __rshift__(self, other):
+        return RShift(self, other)
+
+    def __mod__(self, other):
+        return Mod(self, other)
+
+    def __mul__(self, other):
+        return Mul(self, other)
+
+    def __neg__(self):
+        return Neg(self)
+
+    def __pos__(self):
+        return Pos(self)
+
+    def __pow__(self, other):
+        return Pow(self, other)
+
+    def __sub__(self, other):
+        return Sub(self, other)
+
+    def __truediv__(self, other):
+        return Div(self, other)
+
+    def __or__(self, other):
+        return Or(self, other)
+
+    def __xor__(self, other):
+        return XOr(self, other)
 
 
 class BinaryOp(Expression):
--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -5,6 +5,7 @@ import shutil
 import tempfile
 import operator
 import pytest
+import itertools
 
 import fault
 import magma as m
@@ -128,6 +129,42 @@ def test_binop_two_signals_raw(target, simulator, op):
             tester._circuit.O,
             getattr(operator, op)(tester.peek(tester._circuit.I0_out),
                                   tester.peek(tester._circuit.I1_out)))
+
+    with tempfile.TemporaryDirectory() as _dir:
+        kwargs = {
+            "target": target,
+            "directory": _dir,
+        }
+        if target == "system-verilog":
+            kwargs["simulator"] = simulator
+        tester.compile_and_run(**kwargs)
+
+
+def test_op_tree(target, simulator):
+    class BinaryOpCircuit(m.Circuit):
+        """
+        Pass through I0 and I1 as outputs so we can assert a function
+        of I0 and I1 as the result
+        """
+        IO = ["I0", m.In(m.UInt[5]), "I1", m.In(m.UInt[5]),
+              "I0_out", m.Out(m.UInt[5]), "I1_out", m.Out(m.UInt[5])]
+        IO += ["O", m.Out(m.UInt[5])]
+
+        @classmethod
+        def definition(io):
+            io.I0_out <= io.I0
+            io.I1_out <= io.I1
+            m.wire(io.O, io.I0 + io.I1 & io.I1)
+
+    tester = fault.Tester(BinaryOpCircuit)
+    for _ in range(5):
+        tester.poke(tester._circuit.I0, hwtypes.BitVector.random(5))
+        tester.poke(tester._circuit.I1, hwtypes.BitVector.random(5))
+        tester.eval()
+        expected = tester.peek(tester._circuit.I0_out) + \
+            tester.peek(tester._circuit.I1_out) & \
+            tester.peek(tester._circuit.I1_out)
+        tester.expect(tester._circuit.O, expected)
 
     with tempfile.TemporaryDirectory() as _dir:
         kwargs = {
