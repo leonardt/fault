f71281a61d7ccbb73bebf5069a470f2e732530a1
Lenny Truong <lenny@stanford.edu>
2019-06-07 14:44:29 -0700

    Add tutorial test

---
+++ tutorial/test.py
@@ -0,0 +1,17 @@
+import magma as m
+import fault
+
+
+class Passthrough(m.Circuit):
+    IO = ["I", m.In(m.Bit), "O", m.Out(m.Bit)]
+
+    @classmethod
+    def definition(io):
+        io.O <= io.I
+
+
+passthrough_tester = fault.Tester(Passthrough)
+passthrough_tester.circuit.I = 1
+passthrough_tester.eval()
+passthrough_tester.circuit.O.expect(1)
+passthrough_tester.compile_and_run("verilator")
