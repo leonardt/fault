7aa6353cbbb42c9d5f26fa2f8f848714f6a5ce0b
Steven Herbst <sherbst@fb.com>
2019-08-07 15:25:03 -0700

    add support for real number comparisons

--- fault/__init__.py
+++ fault/__init__.py
@@ -1,9 +1,10 @@
+from .real_type import RealIn, RealOut
+from .elect_type import ElectIn, ElectOut
 from .tester import Tester
 from .power_tester import PowerTester
 from .value import Value, AnyValue, UnknownValue, HiZ
 import fault.random
 from .symbolic_tester import SymbolicTester
-from .extra_types import ElectIn, ElectOut, RealIn, RealOut
 from .verilogams import VAMSWrap
 
 
