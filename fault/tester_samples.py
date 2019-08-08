72b405d7f79ce8a3bfaa4f88c23ae1a002187189
Steven Herbst <sherbst@fb.com>
2019-08-08 14:55:29 -0700

    added supply name flexibility

--- fault/tester_samples.py
+++ fault/tester_samples.py
@@ -2,13 +2,16 @@ import fault
 import random
 
 class SRAMTester(fault.Tester):
-    def __init__(self, circuit, *args, n_trials=100, prob1=0.5, **kwargs):
+    def __init__(self, circuit, *args, n_trials=100, prob1=0.5,
+                 supply1='vdd', supply0='vss', **kwargs):
         # call super constructor
         super().__init__(circuit, *args, **kwargs)
 
         # save settings
         self.n_trials = n_trials
         self.prob1 = prob1
+        self.supply1 = supply1
+        self.supply0 = supply0
 
         # define the test
         self.define_test()
@@ -18,10 +21,10 @@ class SRAMTester(fault.Tester):
         self.poke(self._circuit.wl, 0)
         self.poke(self._circuit.lbl, 0)
         self.poke(self._circuit.lblb, 0)
-        if hasattr(self.circuit, 'vdd'):
-            self.poke(self._circuit.vdd, 1)
-        if hasattr(self.circuit, 'vss'):
-            self.poke(self._circuit.vss, 0)
+        if hasattr(self.circuit, self.supply1):
+            self.poke(getattr(self._circuit, self.supply1), 1)
+        if hasattr(self.circuit, self.supply0):
+            self.poke(getattr(self._circuit, self.supply0), 0)
 
         for _ in range(self.n_trials):
             # generate random input
