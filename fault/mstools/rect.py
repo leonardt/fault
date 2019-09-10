7cb601af415f8a136b8c7f00cdf268d8703f79a4
Steven Herbst <sgherbst@gmail.com>
2019-09-10 11:54:39 -0700

    work in progress

--- fault/mstools/rect.py
+++ fault/mstools/rect.py
@@ -36,6 +36,22 @@ CREATE_LABEL = '''\
 dbCreateLabel({cell_view} list("{layer}" "{purpose}") list({x} {y}) "{text}" "{justify}" "{orient}" "{font}" {height})'''  # noqa
 
 
+def nobus(name):
+    if '<' in name:
+        return name[:name.index('<')]
+    else:
+        return name
+
+
+def busbit(name, bit, bussize):
+    if name is None:
+        return None
+    elif bussize == 1:
+        return f'{name}'
+    else:
+        return f'{name}<{bit}>'
+
+
 def run_skill(skill_cmds, cds_lib):
     cwd = Path(cds_lib).resolve().parent
     with NamedTemporaryFile(dir='.', suffix='.il', mode='w') as f:
@@ -59,12 +75,6 @@ class LayoutLabel:
         self.orient = orient
         self.font = font
         self.height = height
-
-    def add_offset(self, x_off=0, y_off=0):
-        retval = deepcopy(self)
-        retval.x += x_off
-        retval.y += y_off
-        return retval
 
     def create_cmd(self, cell_view):
         # return the label
@@ -144,13 +154,31 @@ class RectCell:
         if schematic_view is None:
             schematic_view = self.schematic_view
 
+        # generate top/bottom connections
+        top = []
+        bottom = []
+        for k in range(nx):
+            for tname in self.top:
+                top.append(busbit(tname, k, nx))
+            for bname in self.bottom:
+                bottom.append(busbit(bname, k, nx))
+
+        # generate left/right connections
+        left = []
+        right = []
+        for k in range(ny):
+            for lname in self.left:
+                left.append(busbit(lname, k, ny))
+            for rname in self.right:
+                right.append(busbit(rname, k, ny))
+
         # compute derived parameters
         width = nx * self.width
         height = ny * self.height
         return RectCell(lib=lib, cell=cell, width=width, height=height,
                         child=self, nx=nx, ny=ny, layout_view=layout_view,
-                        schematic_view=schematic_view, left=self.left,
-                        right=self.right, top=self.top, bottom=self.bottom)
+                        schematic_view=schematic_view, left=left,
+                        right=right, top=top, bottom=bottom)
 
     def write_layout(self):
         # first generate dependencies
@@ -188,19 +216,46 @@ class RectCell:
                 idx += 1
         create_inst = '\n'.join(create_inst)
 
-        # and write labels
+        # write labels
+        nets = []
+
+        # generate top/bottom connections
+        for k in range(self.nx):
+            for tname, bname in zip(self.top, self.bottom):
+                if tname == bname:
+                    bname = None
+                if tname is not None:
+                    net = deepcopy(labels[nobus(tname)])
+                    net.x += k * self.child.width
+                    net.text = busbit(net.text, k, self.nx)
+                    nets.append(net)
+                if bname is not None:
+                    net = deepcopy(labels[nobus(bname)])
+                    net.x += k * self.child.width
+                    net.y += (self.ny - 1) * self.child.height
+                    net.text = busbit(net.text, k, self.nx)
+                    nets.append(net)
+
+        # generate left/right connections
+        for k in range(self.ny):
+            for lname, rname in zip(self.left, self.right):
+                if lname == rname:
+                    rname = None
+                if lname is not None:
+                    net = deepcopy(labels[nobus(lname)])
+                    net.y += k * self.child.height
+                    net.text = busbit(net.text, k, self.ny)
+                    nets.append(net)
+                if rname is not None:
+                    net = deepcopy(labels[nobus(rname)])
+                    net.x += (self.nx - 1) * self.child.width
+                    net.y += k * self.child.height
+                    net.text = busbit(net.text, k, self.ny)
+                    nets.append(net)
+
         create_label = []
-        for lname, rname in zip(self.left, self.right):
-            if lname == rname:
-                lline = labels[lname].create_cmd('parent')
-                create_label.append(lline)
-            else:
-                lline = labels[lname].create_cmd('parent')
-                x_off = self.child.width * (self.nx - 1)
-                rlabel = labels[rname].add_offset(x_off=x_off)
-                rline = rlabel.create_cmd('parent')
-                create_label.append(lline)
-                create_label.append(rline)
+        for net in nets:
+            create_label.append(net.create_cmd('parent'))
         create_label = '\n'.join(create_label)
 
         # generate skill commands
@@ -239,40 +294,70 @@ class RectCell:
         netlist.include(child_netlist)
 
         # determine port information
-        ports = sorted(set(self.left) | set(self.right))
+        ports = set()
+        ports |= set(self.left)
+        ports |= set(self.right)
+        ports |= set(self.top)
+        ports |= set(self.bottom)
+        if None in ports:
+            ports.remove(None)
+        ports = sorted(ports)
 
         # declare the circuit
         netlist.start_subckt(self.cell, *ports)
 
         # declare temporary name
-        def tmpnet(k, count):
-            return f'hpin_{k}_tmp_{count}'
+        def tmpnet(prefix, k, x, y):
+            return f'tmp_{prefix}_{k}_{x}_{y}'
 
+        # instantiate all circuits
         for x in range(self.nx):
-            mapping = {}
+            for y in range(self.ny):
+                # initialize the mapping
+                mapping = {}
+                # wire top/bottom connections
+                for k, (tn, bn) in enumerate(zip(self.child.top,
+                                                 self.child.bottom)):
+                    # handle straight-through wiring
+                    if tn == bn:
+                        mapping[tn] = busbit(tn, x, self.nx)
+                        continue
+                    # wire top
+                    if tn is not None:
+                        if y == 0:
+                            mapping[tn] = busbit(tn, x, self.nx)
+                        else:
+                            mapping[tn] = tmpnet('tb', k, x, y - 1)
+                    # wire bottom
+                    if bn is not None:
+                        if y == self.ny - 1:
+                            mapping[bn] = busbit(bn, x, self.nx)
+                        else:
+                            mapping[bn] = tmpnet('tb', k, x, y)
+                # wire left/right connections
+                for k, (ln, rn) in enumerate(zip(self.left, self.right)):
+                    # handle straight-through wiring
+                    if ln == rn:
+                        mapping[ln] = busbit(ln, y, self.ny)
+                        continue
+                    # wire left
+                    if ln is not None:
+                        if x == 0:
+                            mapping[ln] = busbit(ln, y, self.ny)
+                        else:
+                            mapping[ln] = tmpnet('lr', k, x - 1, y)
+                    # wire right
+                    if rn is not None:
+                        if x == self.nx - 1:
+                            mapping[rn] = busbit(rn, y, self.ny)
+                        else:
+                            mapping[rn] = tmpnet('lr', k, x, y)
+                # create the instance
+                iports = netlist.ordered_ports(mapping=mapping,
+                                               order=child_order)
+                netlist.instantiate(self.child.cell, *iports)
 
-            # wire up LR ports
-            for k, (lname, rname) in enumerate(zip(self.left, self.right)):
-                # special case: wired straight through
-                if lname == rname:
-                    mapping[lname] = lname
-                    continue
-
-                # wire left side
-                if x == 0:
-                    mapping[lname] = lname
-                else:
-                    mapping[lname] = tmpnet(k, x - 1)
-
-                # right side
-                if x == self.nx - 1:
-                    mapping[rname] = rname
-                else:
-                    mapping[rname] = tmpnet(k, x)
-
-            # write the netlist
-            ports = netlist.ordered_ports(mapping=mapping, order=child_order)
-            netlist.instantiate(self.child.cell, *ports)
+        # end the subcircuit definition
         netlist.end_subckt()
 
         # write netlist to file and return path to the netlist
