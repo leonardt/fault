bc117bb4f2e5ace15de02aa62440bf14b4b30e54
Lenny Truong <leonardtruong@fb.com>
2019-12-01 16:40:56 -0800

    Add test file

---
+++ tests/test_system_verilog_target.py
@@ -0,0 +1,20 @@
+import shutil
+import tempfile
+import fault
+import pytest
+from .common import TestBasicClkCircuit
+
+
+def test_vcs_waves():
+    if not shutil.which("vcs"):
+        pytest.skip("Skipping vcs waveform test because vcs is not available")
+    circ = TestBasicClkCircuit
+    tester = fault.Tester(circ, circ.CLK)
+    tester.circuit.I = 0
+    tester.step(2)
+    tester.circuit.I = 1
+    tester.step(2)
+    with tempfile.TemporaryDirectory(dir=".") as _dir:
+        tester.compile_and_run(target="system-verilog", simulator="vcs",
+                               directory=_dir, vcs_waveform_file="waves.vcd")
+
