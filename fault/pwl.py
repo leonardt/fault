bfeba86208fcd552337121cc0161866974356b4a
Daniel Stanley <dstanley@Stanford.edu>
2019-12-04 17:05:46 -0800

    Changed pwc to pwl conversion to allow two points within rt of each other

--- fault/pwl.py
+++ fault/pwl.py
@@ -11,10 +11,31 @@ def pwc_to_pwl(pwc, t_stop, t_tr, init=0):
         t_prev, v_prev = pwc[k - 1]
         t_curr, v_curr = pwc[k]
 
+        if retval[-1][0] >= t_curr:
+            assert retval[-2][0] <= t_curr, \
+                    'non-increasing pwc steps at time' % t_curr
+            if retval[-2][0] == t_curr:
+                # two values at the same time, just drop the earlier
+                #print('dropping old thing')
+                retval.pop()
+                old_t, old_v = retval.pop()
+                v_prev = old_v
+                #print('prev is now', retval[-2:])
+            else:
+                # make the previous thing happen faster than t_tr
+                #print('DOING THE HALFWAY THING')
+                halfway_time = (retval[-2][0] + t_curr)/2
+                retval[-1] = (halfway_time, retval[-1][1])
+
+        #print('times', t_curr, t_curr + t_tr)
         retval += [(t_curr, v_prev)]
         retval += [(t_curr + t_tr, v_curr)]
 
     # add final value
+    # cut off anything after t_stop
+    while len(retval) > 0 and retval[-1][0] >= t_stop:
+        #print('removing one from end')
+        retval.pop()
     retval += [(t_stop, pwc[-1][1])]
 
     # return new waveform
