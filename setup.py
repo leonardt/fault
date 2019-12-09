a11d2a977d77fc601474ead470a94489a7f0103f
Lenny Truong <leonardtruong@fb.com>
2019-12-09 13:11:26 -0800

    Release 2.0.21

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ A Python package for testing hardware (part of the magma ecosystem)\
 
 setup(
     name='fault',
-    version='2.0.20',
+    version='2.0.21',
     description=DESCRIPTION,
     scripts=[],
     packages=[
