fd5427c128243b7bd43ae254aae39979e1d8a724
Keyi Zhang <keyi@stanford.edu>
2019-12-11 21:49:54 -0800

    add kratos debug to verilator

--- tests/test_kratos_debug.py
+++ tests/test_kratos_debug.py
@@ -9,8 +9,15 @@ import os
 import magma
 import shutil
 
+try:
+    import kratos_runtime
+    has_runtime = True
+except ImportError:
+    has_runtime = False
 
-@pytest.mark.skipif(not shutil.which("irun"), reason="irun not available")
+
+@pytest.mark.skipif((not shutil.which("irun")) and not has_runtime,
+                    reason="irun not available")
 def test_load_runtime():
     # define an empty circuit
     mod = kratos.Generator("mod")
@@ -23,6 +30,34 @@ def test_load_runtime():
             tester.compile_and_run(target="system-verilog",
                                    simulator="ncsim",
                                    directory=temp,
+                                   magma_output="verilog",
+                                   use_kratos=True)
+        # run it in a separate process to fake a debugger-simulator interaction
+        p = multiprocessing.Process(target=run_test)
+        p.start()
+        # send an CONTINUE request to the runtime to check if it's working
+        from kratos_runtime import DebuggerMock
+        mock = DebuggerMock()
+        time.sleep(1)
+        mock.connect()
+        mock.continue_()
+        mock.wait_till_finish()
+        p.join()
+
+
+@pytest.mark.skipif(not has_runtime, reason="runtime not available")
+def test_veriltor_load():
+    # define an empty circuit
+    mod = kratos.Generator("mod")
+    with tempfile.TemporaryDirectory() as temp:
+
+        def run_test():
+            # -g without the db dump
+            circuit = kratos.util.to_magma(mod, insert_debug_info=True)
+            tester = fault.Tester(circuit)
+            tester.compile_and_run(target="verilator",
+                                   directory=temp,
+                                   disp_type="realtime",
                                    magma_output="verilog",
                                    use_kratos=True)
         # run it in a separate process to fake a debugger-simulator interaction
