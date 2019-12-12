c3948e1be36d88e45fa770425fbd632f9e66ce2d
Keyi Zhang <keyi@stanford.edu>
2019-12-11 22:13:25 -0800

    add magma combinational tests

--- tests/test_kratos_debug.py
+++ tests/test_kratos_debug.py
@@ -3,17 +3,31 @@ import pytest
 import kratos
 import multiprocessing
 import time
-import pathlib
 import tempfile
-import os
-import magma
+import magma as m
 import shutil
+import operator
+import hwtypes
 
 try:
     import kratos_runtime
     has_runtime = True
 except ImportError:
     has_runtime = False
+
+
+def mock_debugger(fn):
+    # run it in a separate process to fake a debugger-simulator interaction
+    p = multiprocessing.Process(target=fn)
+    p.start()
+    # send an CONTINUE request to the runtime to check if it's working
+    from kratos_runtime import DebuggerMock
+    mock = DebuggerMock()
+    time.sleep(1)
+    mock.connect()
+    mock.continue_()
+    mock.wait_till_finish()
+    p.join()
 
 
 @pytest.mark.skipif((not shutil.which("irun")) and not has_runtime,
@@ -32,17 +46,7 @@ def test_load_runtime():
                                    directory=temp,
                                    magma_output="verilog",
                                    use_kratos=True)
-        # run it in a separate process to fake a debugger-simulator interaction
-        p = multiprocessing.Process(target=run_test)
-        p.start()
-        # send an CONTINUE request to the runtime to check if it's working
-        from kratos_runtime import DebuggerMock
-        mock = DebuggerMock()
-        time.sleep(1)
-        mock.connect()
-        mock.continue_()
-        mock.wait_till_finish()
-        p.join()
+        mock_debugger(run_test)
 
 
 @pytest.mark.skipif(not has_runtime, reason="runtime not available")
@@ -57,17 +61,85 @@ def test_veriltor_load():
             tester = fault.Tester(circuit)
             tester.compile_and_run(target="verilator",
                                    directory=temp,
-                                   disp_type="realtime",
                                    magma_output="verilog",
                                    use_kratos=True)
-        # run it in a separate process to fake a debugger-simulator interaction
-        p = multiprocessing.Process(target=run_test)
-        p.start()
-        # send an CONTINUE request to the runtime to check if it's working
-        from kratos_runtime import DebuggerMock
-        mock = DebuggerMock()
-        time.sleep(1)
-        mock.connect()
-        mock.continue_()
-        mock.wait_till_finish()
-        p.join()
+        mock_debugger(run_test)
+
+
+# TODO: move the function in magma as a normal library call
+def build_kratos_debug_info(circuit, is_top):
+    inst_to_defn_map = {}
+    for instance in circuit.instances:
+        instance_inst_to_defn_map = \
+            build_kratos_debug_info(type(instance), is_top=False)
+        for k, v in instance_inst_to_defn_map.values():
+            key = instance.name + "." + k
+            if is_top:
+                key = circuit.name + "." + key
+            inst_to_defn_map[key] = v
+        inst_name = instance.name
+        if is_top:
+            inst_name = circuit.name + "." + instance.name
+        if instance.kratos is not None:
+            inst_to_defn_map[inst_name] = instance.kratos
+    return inst_to_defn_map
+
+
+@pytest.mark.skipif(not has_runtime, reason="runtime not available")
+@pytest.mark.parametrize("target", ["verilator", "system-verilog"])
+def test_magma_debug(target):
+    if not shutil.which("irun"):
+        pytest.skip("irun not available")
+
+    class SimpleALU(m.Circuit):
+        IO = ["a", m.In(m.UInt[16]), "b", m.In(m.UInt[16]), "c",
+              m.Out(m.UInt[16]), "config_", m.In(m.Bits[2])]
+
+        m.config.set_debug_mode(True)
+
+        @m.circuit.combinational_to_verilog
+        def execute_alu(a: m.UInt[16], b: m.UInt[16], config_: m.Bits[2]) -> \
+                m.UInt[16]:
+            if config_ == m.bits(0, 2):
+                c = a + b
+            elif config_ == m.bits(1, 2):
+                c = a - b
+            elif config_ == m.bits(2, 2):
+                c = a * b
+            else:
+                c = m.bits(0, 16)
+            return c
+
+        @classmethod
+        def definition(io):
+            io.c <= io.execute_alu(io.a, io.b, io.config_)
+
+    inst_to_defn_map = build_kratos_debug_info(SimpleALU, is_top=True)
+    assert "SimpleALU.execute_alu_inst0" in inst_to_defn_map
+    generators = []
+    for instance_name, mod in inst_to_defn_map.items():
+        mod.instance_name = instance_name
+        generators.append(mod)
+
+    tester = fault.Tester(SimpleALU)
+    ops = [operator.add, operator.sub, operator.mul, operator.floordiv]
+    for i, op in enumerate(ops):
+        tester.circuit.config_ = i
+        tester.circuit.a = a = hwtypes.BitVector.random(16)
+        tester.circuit.b = b = hwtypes.BitVector.random(16)
+        tester.eval()
+        if op == operator.floordiv:
+            tester.circuit.c.expect(0)
+        else:
+            tester.circuit.c.expect(op(a, b))
+
+    with tempfile.TemporaryDirectory() as temp:
+
+        def run_test():
+            kwargs = {"target": target, "directory": temp,
+                      "magma_output": "verilog",
+                      "use_kratos": True}
+            if target == "system-verilog":
+                kwargs["simulator"] = "ncsim"
+            tester.compile_and_run(**kwargs)
+        mock_debugger(run_test)
