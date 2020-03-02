7a0d1575a3102fe45db0181ddd02f8410bdbe264
Lenny Truong <leonardtruong@fb.com>
2020-03-02 15:52:58 -0800

    Style

--- fault/magma_utils.py
+++ fault/magma_utils.py
@@ -2,5 +2,5 @@ import magma as m
 
 
 def is_recursive_type(T):
-    return issubclass(T, m.Tuple) or issubclass(T, m.Array) and \
-        not issubclass(T.T, m.Digital)
+    return (issubclass(T, m.Tuple) or issubclass(T, m.Array) and
+            not issubclass(T.T, m.Digital))
