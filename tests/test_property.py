7be92fe9469db77bcb1f13e7f3abd768a4b3c4a7
Leonard Truong <lenny@cs.stanford.edu>
2020-07-01 16:48:30 -0700

    Test xfail

--- tests/test_property.py
+++ tests/test_property.py
@@ -1,3 +1,4 @@
+import pytest
 import fault as f
 import magma as m
 import shutil
@@ -25,4 +26,21 @@ def test_basic_assert():
         tester.circuit.O.expect(1)
         tester.advance_cycle()
         tester.circuit.O.expect(0)
+        tester.compile_and_run("system-verilog", simulator="ncsim", flags=["-sv"])
+
+
+@pytest.mark.xfail
+def test_basic_assert_fail():
+    class Main(m.Circuit):
+        io = m.IO(I=m.In(m.Bits[8]), O=m.Out(m.Bits[8])) + m.ClockIO()
+        io.O @= m.Register(T=m.Bits[8])()(io.I)
+        f.assert_(io.I | f.implies | f.delay[1] | ~io.O, on=f.posedge(io.CLK))
+
+    if shutil.which("ncsim"):
+        tester = f.SynchronousTester(Main, Main.CLK)
+        tester.circuit.I = 1
+        tester.advance_cycle()
+        tester.circuit.I = 0
+        tester.advance_cycle()
+        tester.advance_cycle()
         tester.compile_and_run("system-verilog", simulator="ncsim", flags=["-sv"])
