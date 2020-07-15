749e706db18575b93de34f321b161a3ca0153159
Leonard Truong <lenny@cs.stanford.edu>
2020-07-15 11:15:29 -0700

    Add another failure case test

--- tests/test_property.py
+++ tests/test_property.py
@@ -158,6 +158,12 @@ def test_repetition(sva, capsys):
             tester.circuit.write = 0
             tester.circuit.read = 1
             tester.advance_cycle()
+        # Should fail if we don't see seq2
+        with pytest.raises(AssertionError):
+            tester.compile_and_run("system-verilog", simulator="ncsim",
+                                   flags=["-sv"], magma_opts={"inline": True})
+        out, _ = capsys.readouterr()
+        assert "Assertion Main_tb.dut.__assert_1 has failed" in out
         tester.circuit.write = 0
         tester.circuit.read = 1
         tester.advance_cycle()
@@ -168,5 +174,61 @@ def test_repetition(sva, capsys):
         tester.advance_cycle()
         tester.compile_and_run("system-verilog", simulator="ncsim",
                                flags=["-sv"], magma_opts={"inline": True})
+    else:
+        pytest.skip("need ncsim for SVA test")
+
+
+@pytest.mark.parametrize("sva", [True, False])
+@pytest.mark.parametrize("zero_or_one", [0, 1])
+def test_repetition_or_more(sva, zero_or_one, capsys):
+    # TODO: Parens/precedence with nested sequences (could wrap in seq object?)
+    class Main(m.Circuit):
+        io = m.IO(write=m.In(m.Bit), read=m.In(m.Bit)) + m.ClockIO()
+        if sva:
+            seq0 = f.sva(~io.read, "##1", io.write)
+            seq1 = f.sva(io.read, "##1", io.write)
+            symb = "*" if zero_or_one == 0 else "+"
+            f.assert_(f.sva(seq0, "|-> ##1", io.read, f"[{symb}] ##1", seq1),
+                      on=f.posedge(io.CLK))
+        else:
+            seq0 = ~io.read | f.delay[1] | io.write
+            seq1 = io.read | f.delay[1] | io.write
+            f.assert_(seq0 | f.implies | f.delay[1] | io.read |
+                      f.repeat[zero_or_one:] | f.delay[1] | seq1,
+                      on=f.posedge(io.CLK))
+
+    if shutil.which("ncsim"):
+        for i in range(0, 3):
+            tester = f.SynchronousTester(Main, Main.CLK)
+            tester.circuit.write = 0
+            tester.circuit.read = 0
+            tester.advance_cycle()
+            tester.circuit.write = 1
+            tester.advance_cycle()
+            # Should fail if we don't see seq2
+            with pytest.raises(AssertionError):
+                tester.compile_and_run("system-verilog", simulator="ncsim",
+                                       flags=["-sv"], magma_opts={"inline": True})
+            # do repeated sequence i times
+            for _ in range(i):
+                tester.circuit.write = 0
+                tester.circuit.read = 1
+                tester.advance_cycle()
+            tester.circuit.write = 0
+            tester.circuit.read = 1
+            tester.advance_cycle()
+            tester.circuit.write = 1
+            tester.circuit.read = 0
+            tester.advance_cycle()
+            tester.circuit.write = 0
+            tester.advance_cycle()
+            if i == 0 and zero_or_one == 1:
+                # Should fail on first try (0 times)
+                with pytest.raises(AssertionError):
+                    tester.compile_and_run("system-verilog", simulator="ncsim",
+                                           flags=["-sv"], magma_opts={"inline": True})
+            else:
+                tester.compile_and_run("system-verilog", simulator="ncsim",
+                                       flags=["-sv"], magma_opts={"inline": True})
     else:
         pytest.skip("need ncsim for SVA test")
