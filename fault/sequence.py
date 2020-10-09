1b6e6b889c0cda7d9bf49bd6aaa4e0b9afc73c3f
Lenny Truong <lenny@stanford.edu>
2020-10-09 10:40:04 -0700

    Add prototype sequence tester
    Adds a basic sequence tester pattern drawing off the UVM style pattern
    of sequences, drivers, and monitors.
    
    For now, we simply enable the definitation of input and output sequences
    as a Python Sequence of values.  Drivers and Monitors are functions that
    take a reference to the tester and the current input/output value.
    Inside the function they can lower the value into an arbitrary sequence
    of test actions.
    
    There's definitely more advanced patterns we should consider, such as
    monitors being based purely on dynamic values (e.g. a monitor could just
    watch a bus and make sure transactions are valid using invariants, so it
    wouldn't need to be associated with an output sequence).  There also may
    be variable length sequences (e.g. every 4 input values corresponds to 1
    output value).
    
    However, this basic pattern is intended to target the testing of the
    garnet PE core/tile (thus the test/example is a simple instace of this
    pattern).  The goal is to reuse the same input/output sequence for both
    the core and tile.  This is done by "lifting" the sequences to the tile
    level by defining a new driver (in this case the monitor is the same,
    but for a more complex example, you may need to do some different logic,
    e.g. read the right connection box value based on a randomized
    configuration).
    
    I'm open to changes to the interface (especially with the above examples
    or others in mind), this was simply something basic I came up for the
    target use case.

---
+++ fault/sequence.py
@@ -0,0 +1,64 @@
+from .tester import Tester
+
+
+class Sequence:
+    def __init__(self, values: list):
+        self._values = values
+
+    def __iter__(self):
+        return iter(self._values)
+
+    def __len__(self):
+        return len(self._values)
+
+
+class InputSequence(Sequence):
+    pass
+
+
+class OutputSequence(Sequence):
+    pass
+
+
+class SequenceTester(Tester):
+    def __init__(self, circuit, sequences, clock=None):
+        """
+        sequences: Sequence of pairs (driver/monitor, sequence)
+
+        sequences will be partitioned into input/output sequences
+
+        flow of execution:
+        * run input drivers for next value in each input sequence
+        * eval (no clock) or step(2) (clock)
+        * run output monitors for next value in each output sequence
+
+        will run `n` interations where `n` is the shortest lengths sequence in
+        `sequences`
+        """
+        super().__init__(circuit, clock)
+        inputs = []
+        outputs = []
+        length = None
+        for seq in sequences:
+            if length is None:
+                length = len(seq[1])
+            else:
+                length = min(length, len(seq[1]))
+            if isinstance(seq[1], InputSequence):
+                inputs.append((seq[0], iter(seq[1])))
+            else:
+                assert isinstance(seq[1], OutputSequence)
+                outputs.append((seq[0], iter(seq[1])))
+
+
+        for _ in range(length):
+            for driver, sequence in inputs:
+                driver(self, next(sequence))
+
+            if clock is None:
+                self.eval()
+            else:
+                self.step(2)
+
+            for monitor, sequence in outputs:
+                monitor(self, next(sequence))
---
+++ tests/test_sequence.py
@@ -0,0 +1,78 @@
+import pytest
+
+import magma as m
+from hwtypes import BitVector
+
+from fault.sequence import InputSequence, OutputSequence, SequenceTester
+
+
+class ALUCore(m.Circuit):
+    io = m.IO(a=m.In(m.UInt[16]),
+              b=m.In(m.UInt[16]),
+              opcode=m.In(m.UInt[2]),
+              c=m.Out(m.UInt[16]))
+    io.c @= m.mux([io.a + io.b, io.a - io.b, io.a * io.b, io.a / io.b],
+                  io.opcode)
+
+def core_input_driver(tester, value):
+    a, b, opcode = value
+    tester.circuit.a = a
+    tester.circuit.b = b
+    tester.circuit.opcode = opcode
+
+def shared_output_monitor(tester, value):
+    tester.circuit.c.expect(value)
+
+
+class ALUTile(m.Circuit):
+    io = m.IO(a=m.In(m.UInt[16]),
+              b=m.In(m.UInt[16]),
+              config_data=m.In(m.UInt[2]),
+              config_en=m.In(m.Enable),
+              c=m.Out(m.UInt[16])) + m.ClockIO()
+    config_reg = m.Register(m.Bits[2], has_enable=True)()
+    config_reg.CE @= io.config_en
+    config_reg.I @= io.config_data
+    alu = ALUCore()
+    io.c @= alu(io.a, io.b, config_reg.O)
+
+
+def tile_input_driver(tester, args):
+    a, b, opcode = args
+    tester.circuit.config_en = 1
+    tester.circuit.config_data = opcode
+    tester.step(2)
+    # Make sure enable logic works
+    tester.circuit.config_en = 0
+    tester.circuit.config_data = BitVector.random(2)
+    tester.circuit.a = a
+    tester.circuit.b = b
+
+
+@pytest.mark.parametrize('circuit, input_driver, output_monitor, clock', [
+    (ALUCore, core_input_driver, shared_output_monitor, None),
+    (ALUTile, tile_input_driver, shared_output_monitor, ALUTile.CLK),
+])
+def test_simple_alu_sequence(circuit, input_driver, output_monitor, clock):
+    """
+    Reuse the same input/output sequence for core and tile
+    """
+    inputs = InputSequence([(BitVector.random(16), BitVector.random(16),
+                             BitVector.random(2))
+                            for _ in range(5)])
+
+    ops = [
+        lambda x, y: x + y,
+        lambda x, y: x - y,
+        lambda x, y: x * y,
+        lambda x, y: x // y
+    ]
+    outputs = OutputSequence([ops[int(opcode)](a, b) for a, b, opcode in
+                              inputs])
+
+    tester = SequenceTester(circuit, [
+        (input_driver, inputs),
+        (output_monitor, outputs)
+    ], clock=clock)
+
+    tester.compile_and_run("verilator")
