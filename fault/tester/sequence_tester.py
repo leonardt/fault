e7f7c52d72bb50544099bbd44b506917146e5e3b
Lenny Truong <lenny@stanford.edu>
2020-10-14 16:18:46 -0700

    Refactor based on Raj's design

--- fault/tester/sequence_tester.py
+++ fault/tester/sequence_tester.py
@@ -1,75 +1,34 @@
-from .staged_tester import Tester
-
-
-class Sequence:
-    def __init__(self, values: list):
-        self._values = values
-
-    def __iter__(self):
-        return iter(self._values)
-
-    def __len__(self):
-        return len(self._values)
-
-
-class InputSequence(Sequence):
-    pass
-
-
-class OutputSequence(Sequence):
-    pass
+from fault.tester.staged_tester import Tester
 
 
 class SequenceTester(Tester):
-    def __init__(self, circuit, sequences, clock=None):
-        """
-        sequences: Sequence of pairs (driver/monitor, sequence)
-
-        sequences will be partitioned into input/output sequences
-
-        flow of execution:
-        * run input drivers for next value in each input sequence
-        * eval (no clock) or step(2) (clock)
-        * run output monitors for next value in each output sequence
-
-        will run `n` interations where `n` is the shortest lengths sequence in
-        `sequences`
-        """
+    def __init__(self, circuit, driver, monitor, sequence, clock=None):
         super().__init__(circuit, clock)
-        self.sequences = sequences
+        self.driver = driver
+        self.driver.set_tester(self)
+        self.monitor = monitor
+        self.monitor.set_tester(self)
+        self.sequence = sequence
 
     def _compile_sequences(self):
-        """
-        Sequences are compiled as part of the `_compile` method, this allows
-        the user to record standard tester actions as setup code (e.g. reset,
-        config) before processing the sequences
-        """
-        inputs = []
-        outputs = []
-        length = None
-        for seq in self.sequences:
-            if length is None:
-                length = len(seq[1])
-            else:
-                length = min(length, len(seq[1]))
-            if isinstance(seq[1], InputSequence):
-                inputs.append((seq[0], iter(seq[1])))
-            else:
-                assert isinstance(seq[1], OutputSequence)
-                outputs.append((seq[0], iter(seq[1])))
-
-        for _ in range(length):
-            for driver, sequence in inputs:
-                driver(self, next(sequence))
-
+        for item in self.sequence:
+            self.driver.consume(*item)
             if self.clock is None:
                 self.eval()
             else:
                 self.step(2)
-
-            for monitor, sequence in outputs:
-                monitor(self, next(sequence))
+            self.monitor.observe(*item)
 
     def _compile(self, target="verilator", **kwargs):
         self._compile_sequences()
         super()._compile(target, **kwargs)
+
+
+class Driver:
+    def set_tester(self, tester):
+        self.tester = tester
+
+
+class Monitor:
+    def set_tester(self, tester):
+        self.tester = tester
