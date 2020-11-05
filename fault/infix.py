80a37a2ec26fa68c8f3e1356cc7ce4ef940bdbde
Leonard Truong <lenny@cs.stanford.edu>
2020-11-05 10:01:38 -0800

    Add missing file

---
+++ fault/infix.py
@@ -0,0 +1,12 @@
+from functools import partial
+
+
+class Infix:
+    def __init__(self, func):
+        self.func = func
+
+    def __or__(self, other):
+        return self.func(other)
+
+    def __ror__(self, other):
+        return Infix(partial(self.func, other))
