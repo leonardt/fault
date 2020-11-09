6a330ee7e260376ea63fe18601c9cc100e4e5119
Keyi Zhang <keyi@stanford.edu>
2020-11-09 13:48:43 -0800

    update tb to use wait until high

--- tests/test_fork.py
+++ tests/test_fork.py
@@ -10,7 +10,8 @@ def pytest_generate_tests(metafunc):
 
 def test_fork_join(target, simulator):
     class dut(m.Circuit):
-        io = m.IO(I=m.In(m.Bits[2]), O=m.Out(m.Bits[2]))
+        # add dummy clock
+        io = m.IO(I=m.In(m.Bits[4]), O=m.Out(m.Bits[4]), clk=m.In(m.Clock))
         io.O @= io.I
 
     tester = fault.Tester(dut)
@@ -18,14 +19,17 @@ def test_fork_join(target, simulator):
     proc2 = tester.fork("proc2")
 
     for i, proc in enumerate([proc1, proc2]):
-        proc.poke(tester.circuit.I[i], 1)
+        proc.poke(tester.circuit.I[1 - i], 1)
         proc.eval()
-        proc.expect(tester.circuit.O[i], 1)
+        proc.wait_until_high(tester.circuit.I[i])
+        proc.poke(tester.circuit.I[i + 2], 1)
 
     tester.join(proc1, proc2)
+    tester.eval()
+    tester.expect(tester.circuit.O, 0xF)
 
     tester.compile_and_run(
         target=target,
         simulator=simulator,
-        tmp_dir=True
+        tmp_dir=False
     )
