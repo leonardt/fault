e5ebac8516c9ea79bc941ca81824b5e139187098
Leonard (Lenny) Truong <leonardtruong@gmail.com>
2021-04-14 15:03:40 -0700

    Update test_expressions.py

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -239,7 +239,9 @@ def test_rand():
         io = m.IO(
             read_valid=m.In(m.Bit),
             read_array=m.In(m.Bits[8]),
-            iter_req=m.In(m.Bits[2])) + m.ClockIO()
+            iter_req=m.In(m.Bits[2])
+        ) + m.ClockIO()
+
         f.cover(io.read_valid & (f.countones(io.read_array) == io.iter_req),
                 on=f.posedge(io.CLK))
 
