68b37592575fd53e9498b41fe13446cb78ed7aad
Lenny Truong <lenny@stanford.edu>
2021-06-21 11:56:52 -0700

    Release 3.0.53

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ A Python package for testing hardware (part of the magma ecosystem)\
 
 setup(
     name='fault',
-    version='3.0.52',
+    version='3.0.53',
     description=DESCRIPTION,
     scripts=[],
     packages=[
