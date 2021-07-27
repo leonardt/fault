753bf61fd779d16b236c9a536605f5ef62eb9267
Lenny Truong <lenny@stanford.edu>
2021-07-27 12:38:32 -0700

    Simplify default logic for inline wire prefix

--- fault/assert_immediate.py
+++ fault/assert_immediate.py
@@ -5,7 +5,9 @@ from fault.assert_utils import add_compile_guards
 
 def _make_assert(type_, cond, success_msg=None, failure_msg=None,
                  severity="error", name=None, compile_guard=None, delay=False,
-                 inline_wire_prefix="_FAULT_ASSERT_WIRE_"):
+                 inline_wire_prefix=None):
+    if inline_wire_prefix is None:
+        inline_wire_prefix = "_FAULT_ASSERT_WIRE_"
     success_msg_str = ""
     if success_msg is not None:
         success_msg_str = f" $display(\"{success_msg}\");"
@@ -63,24 +65,21 @@ def _add_docstr(fn):
 
 @_add_docstr
 def assert_immediate(cond, success_msg=None, failure_msg=None, severity="error",
-                     name=None, compile_guard=None,
-                     inline_wire_prefix="_FAULT_ASSERT_WIRE_"):
+                     name=None, compile_guard=None, inline_wire_prefix=None):
     _make_assert("always @(*)", cond, success_msg, failure_msg, severity, name,
                  compile_guard, inline_wire_prefix=inline_wire_prefix)
 
 
 @_add_docstr
 def assert_final(cond, success_msg=None, failure_msg=None, severity="error",
-                 name=None, compile_guard=None,
-                 inline_wire_prefix="_FAULT_ASSERT_WIRE_"):
+                 name=None, compile_guard=None, inline_wire_prefix=None):
     _make_assert("final", cond, success_msg, failure_msg, severity, name,
                  compile_guard, inline_wire_prefix=inline_wire_prefix)
 
 
 @_add_docstr
 def assert_initial(cond, success_msg=None, failure_msg=None, severity="error",
-                   name=None, compile_guard=None,
-                   inline_wire_prefix="_FAULT_ASSERT_WIRE_"):
+                   name=None, compile_guard=None, inline_wire_prefix=None):
     _make_assert("initial", cond, success_msg, failure_msg, severity, name,
                  compile_guard, delay=True,
                  inline_wire_prefix=inline_wire_prefix)
