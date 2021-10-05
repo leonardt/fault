3a516a96de1eb723898d52169db459b51b789157
Lenny Truong <lenny@stanford.edu>
2021-10-05 10:38:33 -0700

    Add -assert logic

--- fault/ready_valid.py
+++ fault/ready_valid.py
@@ -2,12 +2,12 @@ from typing import Mapping
 
 import magma as m
 from fault.tester.staged_tester import Tester
-from fault.assert_immediate import assert_immediate
+from fault.property import assert_, posedge
 
 
 def wrap_with_sequence(ckt, sequences):
     class Wrapper(m.Circuit):
-        io = m.IO(done=m.Out(m.Bit))
+        io = m.IO(done=m.Out(m.Bit)) + m.ClockIO()
         inst = ckt()
         done = m.Bit(1)
         for key, value in sequences.items():
@@ -37,8 +37,8 @@ def wrap_with_sequence(ckt, sequences):
                 # ready (not done with sequence)
                 count.CE @= not_done & port.valid
                 port.ready @= not_done
-                assert_immediate(~(not_done & port.valid) |
-                                 (port.data == curr))
+                assert_(~(not_done & port.valid) | (port.data == curr),
+                        on=posedge(io.CLK))
             else:
                 raise NotImplementedError(port_T)
             # Done when all counters reach final element
@@ -50,10 +50,19 @@ def wrap_with_sequence(ckt, sequences):
 def run_ready_valid_test(ckt: m.DefineCircuitKind, sequences: Mapping,
                          target, synthesizable: bool = True,
                          compile_and_run_args=[], compile_and_run_kwargs={}):
+    if target == "verilator":
+        # Need assert flag for verilator
+        if not compile_and_run_kwargs:
+            compile_and_run_kwargs = {"flags": ["-assert"]}
+        elif not compile_and_run_kwargs.get("flags"):
+            compile_and_run_kwargs["flags"] = ["-assert"]
+        elif "-assert" not in compile_and_run_kwargs["flags"]:
+            compile_and_run_kwargs["flags"].append("-assert")
     if synthesizable:
         wrapped = wrap_with_sequence(ckt, sequences)
         tester = Tester(wrapped)
-        tester.wait_until_high(wrapped.done)
-        tester.compile_and_run(target, *compile_and_run_args, **compile_and_run_kwargs)
+        tester.wait_until_high(wrapped.done, timeout=1000)
+        tester.compile_and_run(target, *compile_and_run_args,
+                               **compile_and_run_kwargs, disp_type="realtime")
         return
     raise NotImplementedError()
