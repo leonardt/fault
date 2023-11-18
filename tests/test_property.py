b3eca7ea4012aeb8f28b7cd7efaf28d3b4b0186d
Lenny Truong <leonardtruong@meta.com>
2023-11-17 19:36:25 -0800

    Fix sequence logic

--- tests/test_property.py
+++ tests/test_property.py
@@ -822,9 +822,9 @@ def test_advanced_property_example_2(use_sva, should_pass):
         if use_sva:
             f.assert_(
                 f.sva(
-                    f.sequence(
+                    f.sequence(f.sva(
                         f.not_(~(io.valid & io.ready.value() & io.eop))
-                    ),
+                    )),
                     "throughout",
                     # Note: need sequences to wrap parens
                     f.sequence(f.sva((io.valid & io.ready.value() & io.sop),
@@ -842,9 +842,8 @@ def test_advanced_property_example_2(use_sva, should_pass):
             )
         else:
             f.assert_(
-                f.sequence(
-                    f.not_(~(io.valid & io.ready.value() & io.eop))
-                ) | f.throughout |
+                f.not_(~(io.valid & io.ready.value() & io.eop))
+                | f.throughout |
                 ((io.valid & io.ready.value() & io.sop) | f.goto[2]),
                 name="eop_must_happen_btn_two_sop_A",
                 on=f.posedge(io.CLK),
