b8f9bb3b894d5d15c534b26a2edbc0f285b9fc02
Lenny Truong <leonardtruong@meta.com>
2023-12-07 06:49:36 -0800

    Use PYPI token

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,8 +6,8 @@ echo "  pypi"                                    >> ~/.pypirc
 echo                                             >> ~/.pypirc
 echo [pypi]                                      >> ~/.pypirc
 echo repository=https://upload.pypi.org/legacy/  >> ~/.pypirc
-echo username=leonardt                           >> ~/.pypirc
-echo password=$PYPI_PASSWORD                     >> ~/.pypirc
+echo username=__token__                          >> ~/.pypirc
+echo password=$PYPI_TOKEN                        >> ~/.pypirc
 
 python setup.py sdist build
 
